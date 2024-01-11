6d30f9b68b1ad0398b53deb621d04521c5a0f0b2
unknown <chandukuruba110@gmail.com>
2024-01-11 10:05:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+    key.addEventListener("animationend",(e)=>e.target.classList.remove("playing"))
+
+  }
+
+  window.addEventListener('keydown', playSound);
+  
+/*
+
+
+
+
+*/
